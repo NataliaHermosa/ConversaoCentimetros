ed55130baceffc359934dfec81ac0dd9c93c7c91
NataliaHermosa <hermozinha@hotmail.com>
2022-07-22 20:14:29 -0300

    Initial commit

---
+++ conversão.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class conversão {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double metros = 0; 
+		double resultado;
+		
+		resultado = metros * 100;
+		
+		System.out.println("Digite o número em metros");
+        metros = scan.nextDouble();
+		System.out.println(resultado);
+		
+		
+		
+		
+		
+				
+		
+				
+	   
+		
+		
+		
+	}
+
+}
